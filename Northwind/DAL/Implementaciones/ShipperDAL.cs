446aee36bbcf974f41dc2062a90b0b43a57d476c
Ari8012 <aaraya50091@ufide.ac.cr>
2025-01-29 10:09:24 -0600

    ADD sin Repositorio Generico.

---
+++ Northwind/DAL/Implementaciones/ShipperDAL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Entities;
+
+namespace DAL.Implementaciones
+{
+    public class ShipperDAL
+    {
+        private NorthWndContext _context;
+
+        public ShipperDAL(NorthWndContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddShipper(Shipper shipper)
+        {
+            try
+            {
+                _context.Add(shipper);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteShipper(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Shipper> GetShippers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateShipper(Shipper shipper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northwind/DAL/Interfaces/IShipperDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Entities;
+
+namespace DAL.Interfaces
+{
+    public interface IShipperDAL
+    {
+        bool AddShipper(Shipper shipper);
+        bool UpdateShipper(Shipper shipper);
+        bool DeleteShipper(int id);
+        List<Shipper> GetShippers();
+    }
+}
