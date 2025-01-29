ecae9957ac4f422dd24efd0200ffdf507336b698
Ari8012 <aaraya50091@ufide.ac.cr>
2025-01-29 10:24:43 -0600

    Creacion de Shipper Service.

---
+++ Northwind/BackEnd/Services/Implementaciones/ShipperService.cs
@@ -0,0 +1,34 @@
+﻿using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementaciones
+{
+    public class ShipperService : IShipperService
+    {
+        private IShipperDAL _shipperDAL;
+        public ShipperService(IShipperDAL shipperDAL)
+        {
+            _shipperDAL = shipperDAL;
+        }
+        public void AddShipper(Shipper shipper)
+        {
+            _shipperDAL.AddShipper(shipper);
+        }
+
+        public void DeleteShipper(Shipper shipper)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Shipper> GetShippers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateShipper(Shipper shipper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northwind/BackEnd/Services/Interfaces/IShipperService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Entities;
+
+namespace BackEnd.Services.Interfaces
+{
+    public interface IShipperService
+    {
+        void AddShipper(Shipper shipper);
+        void UpdateShipper(Shipper shipper);
+        void DeleteShipper(Shipper shipper);
+        List<Shipper> GetShippers();
+
+    }
+}
