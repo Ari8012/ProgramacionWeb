3c15abb95f68b7097f31b59259bd51cb3af4dbda
Ari8012 <aaraya50091@ufide.ac.cr>
2025-02-12 11:23:55 -0600

    ICustomerDAL

---
+++ Northwind/DAL/Interfaces/ICustomerDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Entities;
+
+namespace DAL.Interfaces
+{
+    public class ICustomerDAL : IDALGenerico<Customer>
+    {
+        public bool Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
